b34441d09830e9ff1060255cf10da6ef6452db96
ziadkhaled204 <ziadkhaledyehia@gmail.com>
2026-08-18 11:13:00 +0300

    intiate mock-tamweely

---
+++ mock-tamweely/src/main/java/com/superpay/mock_tamweely/MockTamweelyApplication.java
@@ -0,0 +1,13 @@
+package com.superpay.mock_tamweely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockTamweelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockTamweelyApplication.class, args);
+	}
+
+}
---
+++ mock-tamweely/src/test/java/com/superpay/mock_tamweely/MockTamweelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superpay.mock_tamweely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockTamweelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
